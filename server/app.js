80a9c33972b2f28b076b6632b0b318f3501bf7d7
delviawp <delviawp@gmail.com>
2020-10-30 00:52:09 +0800

    add api key

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-const route = require('./routes/chuckRoute')
+const chuckRoute = require('./routes/chuckRoute')
 const dadRoute = require('./routes/dadJokesRoute')
+const favqRoute = require('./routes/faqRoute')
 const port = 3000
 const errorHandler = require('./middlewares/errorHandler')
 
 app.use(express.urlencoded({extended:true}))
-app.use(route)
+app.use(chuckRoute)
 app.use(dadRoute)
+app.use(favqRoute)
 app.use(errorHandler)
 
 app.listen(port, () => {
---
+++ server/controllers/favqController.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+
+class FavRoute {
+    static async show(req,res, next) {
+        const id = Math.ceil(Math.random() * 10)
+        axios({
+            method: 'get',
+            url: `https://favqs.com/api/quotes/${id}`,
+            headers: {
+                Authorization: "Bearer d23c0ae72c954db1ca8a737f4909c9af"
+            }
+        })
+        .then(({data}) => {
+            console.log(data)
+            res.status(200).json(data.body)
+        })
+        .catch(err => {
+            console.log(err)
+            next(err)
+        })
+    }
+}
+
+module.exports = FavRoute
