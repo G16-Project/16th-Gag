7fca1fed20a993243393291d3b7e2e2288bf7820
delviawp <delviawp@gmail.com>
2020-10-29 21:24:01 +0800

    fixed error handler

--- server/controllers/chuckController.js
+++ server/controllers/chuckController.js
@@ -1,14 +1,14 @@
 const axios = require('axios')
 
 class ChuckController {
-    static async show(req,res) {
+    static async show(req,res, next) {
         try {
             const response = await axios.get('https://api.chucknorris.io/jokes/random');
             //console.log(response.data)
             res.status(200).json({jokes:response.data.value})
           } catch (error) {
             //console.log(error)
-            res.status(500).json(error);
+            next(error);
           }
     }
 }
