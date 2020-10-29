9f8836b5f4a09b859afdf6c0b9eb18dfbca3b225
delviawp <delviawp@gmail.com>
2020-10-29 21:22:25 +0800

    added error handler

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const route = require('./routes/chuckRoute')
 const port = 3000
+const errorHandler = require('./middlewares/errorHandler')
 
 app.use(express.urlencoded({extended:true}))
 app.use(route)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`i love you ${port}`)
